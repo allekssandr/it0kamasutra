277bbb460bbcb7604d5db9f91b18aca524d045b5
Aleksandr <m.allekssandr@gmail.com>
2021-03-17 08:32:00 +0500

    37 урок

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,68 +1,75 @@
-import {rerenderEntireTree} from "../render";
-
-let state = {
-    profilePage: {
-        posts: [
-            {
-                id: 1,
-                message: 'Привет 1',
-                likesCount: 0,
-            },
-            {
-                id: 2,
-                message: 'Привет 2',
-                likesCount: 0,
-            },
-            {
-                id: 3,
-                message: 'Привет 3',
-                likesCount: 0,
-            },
-            {
-                id: 4,
-                message: 'Привет 4',
-                likesCount: 0,
-            },
-        ],
-        newPostText: '',
+let store = {
+    _state: {
+        profilePage: {
+            posts: [
+                {
+                    id: 1,
+                    message: 'Привет 1',
+                    likesCount: 0,
+                },
+                {
+                    id: 2,
+                    message: 'Привет 2',
+                    likesCount: 0,
+                },
+                {
+                    id: 3,
+                    message: 'Привет 3',
+                    likesCount: 0,
+                },
+                {
+                    id: 4,
+                    message: 'Привет 4',
+                    likesCount: 0,
+                },
+            ],
+            newPostText: '',
+        },
+        dialogsPage: {
+            dialogs: [
+                {id: 1, name: 'Dima'},
+                {id: 2, name: 'Sasha'},
+                {id: 3, name: 'Masha'},
+                {id: 4, name: 'Alena'},
+                {id: 5, name: 'Ivan'},
+            ],
+            messages: [
+                {id: 1, message: 'Hi!'},
+                {id: 2, message: 'How it you work&'},
+                {id: 3, message: 'Yo!'},
+            ],
+        },
+        sidebar: {
+            friendsBar: [
+                {id: 1, name: 'Ivan'},
+                {id: 2, name: 'Dima'},
+                {id: 3, name: 'Sasha'},
+            ]
+        }
     },
-    dialogsPage: {
-        dialogs: [
-            {id: 1, name: 'Dima'},
-            {id: 2, name: 'Sasha'},
-            {id: 3, name: 'Masha'},
-            {id: 4, name: 'Alena'},
-            {id: 5, name: 'Ivan'},
-        ],
-        messages: [
-            {id: 1, message: 'Hi!'},
-            {id: 2, message: 'How it you work&'},
-            {id: 3, message: 'Yo!'},
-        ],
+    getState() {
+        return this._state
     },
-    sidebar: {
-        friendsBar: [
-            {id: 1, name: 'Ivan'},
-            {id: 2, name: 'Dima'},
-            {id: 3, name: 'Sasha'},
-        ]
-    }
+    _callSubscriber() {
+        console.log('state change');
+    },
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    addPost(){
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likesCount: 0,
+        };
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    subscribe(observer){
+        this._callSubscriber = observer;
+    },
 }
 
-export let addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likesCount: 0,
-    }
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireTree(state);
-}
-
-export let updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-}
-
-export default state;
+export default store;
